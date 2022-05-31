262f1f2dc54a6a1588e198938b1da9d9a83b191b
EvaAst <belorusova.e@gmail.com>
2022-05-31 22:53:32 +0700

    Add files

--- 1seq.py
+++ 1seq.py
@@ -1,10 +1,10 @@
-amount= input('Введите количество элементов ')
-numbers=int(amount)
+amount = input('Введите количество элементов ')
+numbers = int(amount)
 result = []
 
 for i in range(numbers):
-    number=int(input('Введите элемент: '))
-    result.append(number) #append добавляет элемент в конец
+    number = int(input('Введите элемент: '))
+    result.append(number)  # append добавляет элемент в конец
 
 result.sort()
 print(result)
--- 2seq.py
+++ 2seq.py
@@ -1,9 +1,9 @@
-entering_numbers= input('Введите любые цифры через запятую ')
+entering_numbers = input('Введите любые цифры через запятую ')
 # если ввод разными знаками используем метод replace, разделить элементы помогает split
-numbers =(entering_numbers.replace(';',',').replace(',', ' ').split())
+numbers = (entering_numbers.replace(';', ',').replace(',', ' ').split())
 
 result = []
 for i in numbers:
-    if numbers.count(i) ==1:
-        result.append(i) #append добавляет элемент в конец
+    if numbers.count(i) == 1:
+        result.append(i)  # append добавляет элемент в конец
 print(result)
