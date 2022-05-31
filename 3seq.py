dfb39e8ef1fa15fd78b4b50961675667a29b780f
EvaAst <belorusova.e@gmail.com>
2022-05-31 23:11:45 +0700

    добавлен "/"

--- 3seq.py
+++ 3seq.py
@@ -1,8 +1,8 @@
 entering_numbers = input('Введите элементы 1-го списка: ')
 input_numbers = input('Введите элементы 2-го списка: ')
 # если ввод разными знаками используем метод replace, разделить элементы помогает split
-numbers = (entering_numbers.replace(';', ',').replace(',', ' ').split())
+numbers = (entering_numbers.replace(';', ',').replace('/', ',').replace(',', ' ').split())
 
-amount = (input_numbers.replace(';', ',').replace(',', ' ').split())
+amount = (input_numbers.replace('/', ',').replace(';', ',').replace(',', ' ').split())
 
 print(set(numbers) - set(amount))
